bb3bb83319dde4cbbdd23454554957d28fd15f49
Dhruv Ahlawat <96519848+DhruvAhlawat@users.noreply.github.com>
2022-11-22 17:53:08 +0530

    Added Animation Capability
    can render the animations now, and also save them in the .gif format.

--- Wavefunctions.py
+++ Wavefunctions.py
@@ -2,37 +2,39 @@ import numpy as np;
 from math import *;
 from matplotlib import pyplot as plt;
 from matplotlib.animation import FuncAnimation;
-def f(x):
-    if(0 < x <= 20):
-        return sin(x)/x;
-    if(x == 0):
-        return 1;
-    else:
-        return 0;
+import matplotlib.animation as animationFunctions;
 
-#custom integrator that uses the riemann summation technique to integrate any function
+
+#if saving is needed then use Cntrl+F to search for "anim.save" and
+
+
+
 class MyQuantumBox:
-    a = 10;
     fineness = 1000; #0 is the left limit of the box and x=a is the right limit of the box
     num = 50; #the maximum n it will go to with sine functions
     exponentConst = 1;
-    timeLimit = 1;
-    timeDelay = 0.1; #waits for this much seconds each frame
-    timeSamples = int(ceil(timeLimit/timeDelay));
-    x = np.linspace(0,a,ceil(a*fineness));
-    xPoints = np.linspace(0,a,ceil(a*100));
-
+    timeLimit = 5;
+    timeDelay = 0.05; #waits for this much seconds each frame
+    timeSamples = int(floor(timeLimit/timeDelay));
+    #x = np.linspace(0,a,ceil(a*fineness));
+   # xPoints = np.linspace(0,a,ceil(a*100));
+    originalYval = [];
     cn = [0]*(num+1); #cn[i] stores the coeffecients corresponding to sin((i*pi/a)x), or the (i-1)th excited
             #state of the wavefunction 
-    t = [0]*(timeSamples);
+    t = np.linspace(0,timeLimit,timeSamples);
     #func = function;
-    
-   
-    def __init__(self,a,num = 50,fineness = 1000) -> None:
+    RealPart = []; 
+    ImaginaryPart = [row[:] for row in RealPart]; 
+    Modulus = [row[:] for row in RealPart]; 
+    maxUpperYbound = 0;
+    def __init__(self,a,func,timeLimit = 5, speed = 20, num = 50,fineness = 1000) -> None:
         self.a = a;
         self.fineness = fineness;
         self.num = num;
         #self.func = func;
+        self.timeLimit = timeLimit;
+        self.timeSamples = int(floor(timeLimit/self.timeDelay));
+        self.initialise(func,speed);
     #general function to integrate things, can be used without instantiating object
     def Integrate (func, xlimL,xlimR,fineness = 1000):
         #func is a function of one variable, that returns a floating point value
@@ -55,13 +57,25 @@ class MyQuantumBox:
 
         return sum;
     
-    def initialise(self,func):
-        self.exponentConst = np.pi*np.pi*1; #for now
+    def initialise(self,func, speed=10):
+        self.exponentConst = speed*0.0005718734319419223; #for now
         self.cn = [0]*(self.num+1);
         self.xPoints = np.linspace(0,self.a,ceil(self.a*100));
         self.t = np.linspace(0,self.timeLimit,self.timeSamples);
         self.x = np.linspace(0,self.a,ceil(self.a*self.fineness));
         self.CalculateAllCoeffecients(func);
+        
+        for i in self.xPoints:
+            self.originalYval.append(func(i));
+        self.RealPart = []; 
+        for i in range(0,self.t.size):
+            cur = [0]*self.xPoints.size; 
+            self.RealPart.append(cur); 
+        self.ImaginaryPart = [row[:] for row in self.RealPart]; 
+        self.Modulus = [row[:] for row in self.RealPart]; 
+        self.CalculateAllPartsForTime();
+
+        
     def CalculateNthCoeffecient(self,func,n):
         sum = 0; 
         for i in self.x:
@@ -75,14 +89,26 @@ class MyQuantumBox:
         for i in range(1,self.num+1):
             self.CalculateNthCoeffecient(func,i);
     
-    
     def initialFunction(self,x):
         ans = 0;
         for i in range(0,self.num):
             ans += self.cn[i]*sin((pi*i*x)/self.a)
         ans *= sqrt(2/self.a);
         return ans;
+    def RealPartFunction(self,x,t):
+        ans = 0;
+        for i in range(0,self.num):
+            ans += self.cn[i]*sin((pi*i*x)/self.a)*cos(self.exponentConst*t*(i**2));
+        ans *= sqrt(2/self.a);
+        return ans;
     #anim = FuncAnimation(fig,AnimateFrames,frames=t,init_func=init, blit=True);
+    def ImaginaryPartFunction(self,x,t):
+        ans = 0;
+        for i in range(0,self.num):
+            ans += self.cn[i]*sin((pi*i*x)/self.a)*sin(-self.exponentConst*t*(i**2));
+        ans *= sqrt(2/self.a);
+        return ans;
+
     def createPlotInitial(self):
         y = [];
         for i in self.xPoints:
@@ -101,14 +127,70 @@ class MyQuantumBox:
         # anim = FuncAnimation(self.fig,self.AnimateFrames,
         #  frames=self.timeSamples,interval = self.timeDelay
         # );
+    
+    # RealPart = []; 
+    # for i in range(0,t.size):
+    #     cur = [0]*xPoints.size; 
+    #     RealPart.append(cur); 
+    # ImaginaryPart = [row[:] for row in RealPart]; 
+    # Modulus = [row[:] for row in RealPart]; #effectively deep copies
+    
+    #now we must calculate the real part of the function as well as the imaginary part of the function at different 
+    #input times in the numpy list t
+    def CalculateAllPartsForTime(self):
+        # print(len(self.RealPart));
+        # print(len(self.RealPart[0]));
+        for i in range(0,self.t.size):
+            #for all of the times calculate the values
+            for j in range(0, self.xPoints.size):
+                self.RealPart[i][j] = self.RealPartFunction(self.xPoints[j],self.t[i]);
+                self.ImaginaryPart[i][j] = self.ImaginaryPartFunction(self.xPoints[j],self.t[i]);
+                self.Modulus[i][j] = sqrt(self.RealPart[i][j]**2 + self.ImaginaryPart[i][j]**2);
+                if(self.Modulus[i][j] > self.maxUpperYbound):
+                    self.maxUpperYbound = self.Modulus[i][j]; 
+                # if(self.Modulus[i][j] < self.maxLowerYBound):
+                #     self.maxLowerYBound = self.Modulus[i][j];
+        
+    def DisplayAnimation(self):
+        fig, ax = plt.subplots(); fig.set_size_inches(10,6);
+        ax.set_xlim(0,a); ax.set_ylim(-self.maxUpperYbound*1.1,self.maxUpperYbound*1.1);
+
+        line = ax.plot(self.xPoints,self.RealPart[0][:],color = 'k', lw=1, label = "Real Part Re(Psi)")[0];
+        ModLine = ax.plot(self.xPoints,self.Modulus[0][:],color = 'r', lw=2,label="|Psi|")[0];
+        ImLine = ax.plot(self.xPoints,self.ImaginaryPart[0][:],color = 'b', lw=1, label="Imaginary part Im(Psi)")[0];
+        def animate(i):
+            line.set_ydata(self.RealPart[i][:]);
+            ModLine.set_ydata(self.Modulus[i][:]);
+            ImLine.set_ydata(self.ImaginaryPart[i][:]);
+        originalFunction, = ax.plot(self.xPoints,self.originalYval,'b--', label="Psi at T=0");
+        ax.legend();
+        anim = FuncAnimation(fig,animate, interval = self.timeDelay*1000, frames=len(self.t)-1);
+        saveLocation = r"D:\OneDrive - IIT Delhi\Pictures\_PythonProjects\Wavefunctions\Animations of Wavefunction\anim50.gif";
+        # set the above save location correctly
+        #for some reason, the r must be placed in front of the path
+
+        Writer = animationFunctions.FFMpegWriter(fps=30);
+        anim.save(saveLocation,writer='imagemagick');
+        plt.draw();
+        plt.show();
+
         
 
 
-a = 10;
-box1 = MyQuantumBox(a);
-box1.initialise(f);
-
-#xPoints = np.linspace(0,a,ceil(a*100));
-plt.xlabel("x coordinate",color='red');
-#box1.CalculateAllParts(f);
-box1.createPlotInitial();
+a = 4;
+def f(x):
+    if(1.98 <= x <= 2.02):
+        return 1;
+    if(x == 0):
+        return 1;
+    else:
+        return 0;
+def CentredAt2(x):
+    a = 1.85; b = 2.15;
+    if(a <= x <= b):
+        return sin((x-a)*pi/(b-a));
+    else:
+        return 0;
+box1 = MyQuantumBox(a,CentredAt2,50,25);
+#box1.initialise(CentredAt2,20);
+box1.DisplayAnimation();
